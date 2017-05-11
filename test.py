7a8ce495dc9f076db9c03333a2efc6da1fba7747
Nikos Giachoudis <ngiachou@gmail.com>
2017-05-11 18:42:05 +0300

    Code revision and test.py now can be imported without running tests by default

--- test.py
+++ test.py
@@ -30,23 +30,24 @@ def tester(p, data):
 	print "false negative: "+str(false_negative)
 	print "Summation     : "+str(true_positive+true_negative+false_positive+false_negative)
 
-p = Perceptron([1,1,1,1])
-print "Testing perceptron with weight vector: "+str(p.getWeightVector())
-print "Result for input vector (255,100,30,1): "+str(p.fire([255,100,30,1]))
-print "Result for input vector (200,110,50,1): "+str(p.fire([200,110,50,1]))
-print "Result for input vector (30,100,200,1): "+str(p.fire([30,100,200,1]))
-print "Result for input vector (120,235,12,1): "+str(p.fire([120,235,12,1]))
-print "Result for input vector (40,100,30,1): "+str(p.fire([40,100,30,1]))
-print "Result for input vector (155,55,100,1): "+str(p.fire([155,55,100,1]))
-print "Result for input vector (255,200,55,1): "+str(p.fire([255,200,55,1]))
+if __name__ == '__main__':
+	p = Perceptron([1,1,1,1])
+	print "Testing perceptron with weight vector: "+str(p.getWeightVector())
+	print "Result for input vector (255,100,30,1): "+str(p.fire([255,100,30,1]))
+	print "Result for input vector (200,110,50,1): "+str(p.fire([200,110,50,1]))
+	print "Result for input vector (30,100,200,1): "+str(p.fire([30,100,200,1]))
+	print "Result for input vector (120,235,12,1): "+str(p.fire([120,235,12,1]))
+	print "Result for input vector (40,100,30,1): "+str(p.fire([40,100,30,1]))
+	print "Result for input vector (155,55,100,1): "+str(p.fire([155,55,100,1]))
+	print "Result for input vector (255,200,55,1): "+str(p.fire([255,200,55,1]))
 
-data = readFile("rgb.csv")
-# for debugging
-# print data
-t = Trainers(data,0.7)
-#raw_input("Continue?")
-t.gradientDescent(p)
-print "Perceptron's weight vector after training: "+str(p.getWeightVector())
+	data = readFile("rgb.csv")
+	# for debugging
+	# print data
+	t = Trainers(data,0.7)
+	#raw_input("Continue?")
+	t.gradientDescent(p)
+	print "Perceptron's weight vector after training: "+str(p.getWeightVector())
 
-data = readFile("rgb_test.csv")
-tester(p,data)
+	data = readFile("rgb_test.csv")
+	tester(p,data)
